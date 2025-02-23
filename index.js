46ab4e692244ab92d4efceb97fa4b3c87f83e9d6
houssam eddine <houssameddineel@gmail.com>
2025-02-23 13:16:59 +0000

    adding popUp

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const taskInput = document.querySelector(".taskInput");
 const addButton = document.querySelector(".add");
 const searchInput = document.querySelector(".searchInput");
 const clearButton = document.querySelector(".clear");
+const popUp = document.querySelector(".popUp");
+const popUpYesButton = document.querySelector(".popUpContainer").childNodes[3];
+const popUpNoButton = document.querySelector(".popUpContainer").childNodes[5];
 const liContainer = document.querySelector(".liContainer");
 
 addButton.addEventListener("click",createLi);
+
+clearButton.addEventListener("click",()=>{revealPopUp("block");});
+
+popUpYesButton.addEventListener("click",removeAllLi);
+
+popUpNoButton.addEventListener("click",()=>{revealPopUp("none");});
 
 function createLi()
 {
@@ -20,9 +29,26 @@ function createLi()
 
     let buttonElement = document.createElement("button");
     buttonElement.innerHTML = "X";
+    buttonElement.addEventListener("click",removeLi);
     
     liElement.appendChild(buttonElement);
     liContainer.appendChild(liElement);
 
     taskInput.value = "";
+}
+
+function removeLi(xButton)
+{
+    xButton.target.parentElement.remove();
+}
+
+function revealPopUp(state)
+{
+    popUp.style.display = state;
+}
+
+function removeAllLi()
+{
+    liContainer.innerHTML = "";
+    revealPopUp("none");
 }
